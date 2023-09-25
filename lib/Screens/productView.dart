d2e5a213bf3901598b023c9aa0341b888a3fe4a7
atifkhan77 <atifm1190@gmail.com>
2023-09-25 23:47:51 +0500

    add filter as fav

--- lib/Screens/productView.dart
+++ lib/Screens/productView.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/prorduct_grid.dart';
 
-class ProductView extends StatelessWidget {
+enum FilterOption {
+  Favorites,
+  All,
+}
+
+class ProductView extends StatefulWidget {
   ProductView({super.key});
 
   @override
+  State<ProductView> createState() => _ProductViewState();
+}
+
+class _ProductViewState extends State<ProductView> {
+  var _showOnlyFavorites = false;
+  @override
   Widget build(BuildContext context) {
+    // final productContainer =
+    //     Provider.of<ProductProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: Text("eShop"),
+        title: const Text("eShop"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOption seletedValue) {
+              setState(() {
+                if (seletedValue == FilterOption.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOption.Favorites,
+                child: Text('Only Favorites'),
+              ),
+              const PopupMenuItem(
+                value: FilterOption.All,
+                child: Text('Show All'),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductGrid(),
+      body: ProductGrid(
+        _showOnlyFavorites,
+      ),
     );
   }
 }
